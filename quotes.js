be69afab423a2ebb3648ff27e5a1f522c88a66d0
Abhishek Samariya <asamariya@gmail.com>
2019-11-09 10:32:28 +1100

    Added styles - If the quote is longer than 100 chars long, reduce h1 size to 40px - Adjust h1 indentation - Change background color every time you refresh the page or click the shuffle btn

--- quotes.js
+++ quotes.js
@@ -1,6 +1,7 @@
 const quoteTag = document.querySelector('h1');
 const authorTag = document.querySelector('#author');
 const randomBtn = document.querySelector('footer img');
+const bodyTag = document.querySelector('body');
 
 const url = 'https://api.superhi.com/api/test/quotes/random';
 
@@ -9,8 +10,16 @@ async function getQuote() {
 		let response = await fetch(url);
 		let data = await response.json();
 
-		quoteTag.innerHTML = data.quote;
-		authorTag.innerHTML = data.author;
+		quoteTag.innerHTML = `&ldquo;${data.quote}&rdquo;`;
+		authorTag.innerHTML = `&mdash; ${data.author}`;
+
+		if (data.quote.length > 100) {
+			quoteTag.classList.add('long');
+		} else {
+			quoteTag.classList.remove('long');
+		}
+
+		bodyTag.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`;
 	} catch (error) {
 		console.log(error);
 	}
